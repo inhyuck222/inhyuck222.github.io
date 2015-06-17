591ce267d2ed980411364955c891701552c6859d
Inhyuck Im <inhyuck222@users.noreply.github.com>
2015-06-18 01:47:38 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,7 @@ $(function() {
 
 	var div_session = "<div id='facebook-session'>"+
 					  "<strong></strong>"+
-					  "<img class="img-responsive" alt="Responsive image">"+
+					  //"<img>"+
 					  "<a href='#' id='logout' class='btn btn-danger btn'>Logout</a>"+
 					  "</div>";
 
@@ -72,11 +72,8 @@ $(function() {
 				})
 			}
   		})
-
   	}
-
-
-
+  	
   	$(document).on('click', '#login', function(e) {
   		e.preventDefault();
   		facebookLogin();
@@ -88,102 +85,3 @@ $(function() {
   	})
 
 })
-
-
-/*
-$(function(){
-
-	var app_id = '426904250826355';
-	var scopes = 'email, user_friends, user_online_presence';
-
-	var btn_login = '<a href = "#" id = "login" class = "btn btn-primary">Iniciar sesion></a>';
-
-	var div_session = "<div id = 'facebook-session'>"+
-					  "<strong></strong>"+
-					  "<img>"+
-					  "<a href = '#' id = 'logout' class = 'btn btn-danger'>Cerrar session</a>"+
-					  "</div>";
-
-	window.fbAsyncInit = function() {
-	
-		FB.init({
-			appId      : app_id,
-			status 	   : true,
-			cookie     : true,
-			xfbml      : true,
-			version    : 'v2.1'
-		});	
-
-		FB.getLoginStatus(function(response) {
-			statusChangeCallback(response, function(){
-
-			});
-		});
-
-	};
-	var statusChangeCallback = function(response, callback) {
-    	console.log(response);
-   
-    	if (response.status === 'connected') {
-    		getFacebookData();
-    	} else {
-        	callback(false);
-    	}
-  	}
-
-  	var checkLoginState = function(callback) {
-    	FB.getLoginStatus(function(response) {
-    		statusChangeCallback(response, function(data){
-    			callback(data);
-    		});
-   		});
-  	}
-
-  	var getFacebookData = function(){
-  		FB.api('/me', function(response){
-  			$('#login').after(div_session);
-  			$('#login').remove();
-  			$('#facebook-session strong').text("Bienvenido: "+response.name);
-  			$('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
-  		});
-  	}
-  	var facebookLogin = function(){
-  		checkLoginState(function(response){
-  			if (!response) {
-  				console.log(response);
-  				FB.login(function(response) {
-  					if(response.status === 'connected')
-  						getFacebookData();
-  				}, {scope: scopes});
-  			}
-  		})
-  	}
-
-  	var facebookLogout = function() {
-  		FB.getLoginStatus(function(response) {
-  			if(response.status === 'connected'){
-  				FB.logout(function(response){
-  					$('#facebook-session').before(btn_login);
-  					$('#facebook-session').remove();
-  				})
-  			}
-  		});
-  	}
-
-  	$(document).on('clikc', '#login', function(e){
-  		e.preventDefault();
-
-  		facebookLogin();
-  	})
-
-  	$(document).on('clikc', '#logout', function(e){
-  		e.preventDefault();
-
-  		if(confirm("logout?"))
-  		facebookLogout();
-  		else return false;
-  	})
-
-
-})
-*/
